771757822b1ab2a779df772cff19d7700b6c0ba6
hegenova <84971258+hegenova@users.noreply.github.com>
2022-10-29 21:20:00 +0700

    Delete jawaban directory

--- jawaban/soal5.sh
+++
@@ -1,6 +0,0 @@
-echo '
-zone "wise.b02.com" {
-    type slave;
-    masters { 10.4.2.2; }; // Masukan IP Wise tanpa tanda petik
-    file "/var/lib/bind/wise.b02.com";
-}; ' > /etc/bind/named.conf.local
